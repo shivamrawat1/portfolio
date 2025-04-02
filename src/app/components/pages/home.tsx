748369cd6406feccd25c670db0c4202f43ccbed5
shivam <shivam@uni.minerva.edu>
2025-04-01 23:37:00 -0700

    fixed hero image

--- src/app/components/pages/home.tsx
+++ src/app/components/pages/home.tsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 export default function HomePage() {
     return (
         <div className={styles.home_container}>
-            <div className={styles.content_area}>
-                <div className={styles.hero_image}>
-                    <Image
-                        src="/file1.svg"
-                        alt="File illustration"
-                        width={800}
-                        height={100}
-                        priority
-                        className={styles.file_svg}
-                    />
-                </div>
+            <div className={styles.hero_image}>
+                <Image
+                    src="/file1.svg"
+                    alt="File illustration"
+                    width={950}
+                    height={100}
+                    priority
+                    className={styles.file_svg}
+                />
             </div>
         </div>
     );
