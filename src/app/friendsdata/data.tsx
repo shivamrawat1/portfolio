0accc11c21fc2a4e3e41a797c48a16227b623e51
shivam <shivam@uni.minerva.edu>
2025-10-30 15:49:51 -0700

    added more friends data

--- src/app/friendsdata/data.tsx
+++ src/app/friendsdata/data.tsx
@@ -54,7 +54,103 @@ export const friendsData = [
         color: "#fceadd",
         link: "https://www.linkedin.com/in/jeevancs/",
         image: "/headshots/Jeevan Bhatta.jpg"
-    }
+    },
+    {
+        id: 8,
+        name: "Marta Borrell Fijo",
+        tags: ["Travel", "Food", "Education", "Social Good", "Sustainibility"],
+        color: "#fceadd",
+        link: "https://www.instagram.com/martabf_12/",
+        image: "/headshots/Marta Borrell Fijo.jpg"
+    },
+    {
+        id: 9,
+        name: "Jun Qi Liew",
+        tags: ["Music", "Food", "Education", "Science", "Gaming"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/jun-qi-liew/",
+        image: "/headshots/Jun Qi Liew.jpg"
+    },
+    {
+        id: 10,
+        name: "Len Park",
+        tags: ["Technology", "Food", "Sports", "Business", "Gaming"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/minseo-len-park/",
+        image: "/headshots/Minseo Park.jpeg"
+    },
+    {
+        id: 11,
+        name: "Maggie Possidente",
+        tags: ["Travel", "Art", "Nature", "Education", "Social Good"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/maggie-possidente/",
+        image: "/headshots/Maggie Possidente.png"
+    },
+    {
+        id: 12,
+        name: "Yuzzy Itaba",
+        tags: ["Travel", "Food", "Fashion", "Sports", "Business"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/yuzuki-itaba-a20000259/",
+        image: "/headshots/Yuzzy Itaba.png"
+    },
+    {
+        id: 13,
+        name: "Narandelger Erdenebileg",
+        tags: ["Travel", "Food", "Fashion", "Sports", "Business"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/narandelger-erdenebileg/",
+        image: "/headshots/Narandelger Erdenebileg.jpeg"
+    },
+    {
+        id: 14,
+        name: "Maja Ostrowska",
+        tags: ["Travel", "Sports", "Nature", "Social Good", "Business"],
+        color: "#fceadd",
+        link: "http://linkedin.com/in/maja-ostrowska-b91ba9204/",
+        image: "/headshots/Maja Ostrowska.jpeg"
+    },
+    {
+        id: 15,
+        name: "Saad Asad",
+        tags: ["Travel", "Technology", "Writing", "Nature", "Design"],
+        color: "#fceadd",
+        link: "https://x.com/Saad_163_/",
+        image: "/headshots/Muhammad Asad.jpeg"
+    },
+    {
+        id: 16,
+        name: "Outdomchakriya Soth",
+        tags: ["Food", "Art", "Education", "Social Good", "Gaming"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/riyasoth/",
+        image: "/headshots/Outdomchakriya Soth.png"
+    },
+    {
+        id: 17,
+        name: "Tedy Lewis",
+        tags: ["Fitness", "Music", "Food", "Nature", "Sustainibility"],
+        color: "#fceadd",
+        link: "https://www.instagram.com/tedy_lewis/",
+        image: "/headshots/Douglas Lewis.jpeg"
+    },
+    {
+        id: 18,
+        name: "Iram Inamdar",
+        tags: ["Travel", "Writing", "Nature", "Science", "Sustainibility"],
+        color: "#fceadd",
+        link: "https://www.linkedin.com/in/iram-inamdar/",
+        image: "/headshots/Iram Ikbal Inamdar.jpg"
+    },
+    {
+        id: 19,
+        name: "Thu Than",
+        tags: ["Technology", "Food", "Writing", "Education", "Design"],
+        color: "#fceadd",
+        link: "https://thu-portfolio-five.vercel.app/",
+        image: "/headshots/Thu Than.jpeg"
+    },
 ];
 
 
